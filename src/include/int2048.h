1f47d25ed27b42752c48ca55f3079e19f388ee8e
StarryPurple <starrypurple@163.com>
2024-11-14 16:49:30 +0800

    NTT finished

--- src/include/int2048.h
+++ src/include/int2048.h
@@ -18,14 +18,57 @@
 // 请不要使用 using namespace std;
 
 namespace sjtu {
+  // 采用 BASE = 10000 进制
 class int2048 {
-  // todo
+
+  static constexpr int INT2048_BASE = 10000;
+  // static constexpr int INT2048_MAX_DIGIT = 1000000;
+  static constexpr double PI = 3.14159265358979324;
+  static constexpr int INT2048_MAX_DIGIT_POWER = 20;
+  static constexpr int INT2048_MAX_DIGIT = 1 << (INT2048_MAX_DIGIT_POWER + 1);
+  static int FFT_NTT_rev[INT2048_MAX_DIGIT];
+  static constexpr long long mod1 = 469762049, mod2 = 167772161, g_mod = 3,
+  inv1 = 130489458, inv2 = 104391568;
+
+  struct Complex;
+
 public:
+  using storage_type = int;
+  std::vector<storage_type> storage;  // 存储数字的绝对值
+  int storage_sign;  // 正负指示， 1 为正， -1 为负，存储的数为0时指示符为1
+
+  bool is_zero() const;
+  static storage_type lower_half(storage_type);  // 进位辅助函数：无需进位部分。
+  static storage_type upper_half(storage_type);  // 进位辅助函数：待进位部分。
+  static storage_type lower_half(long long);  // 进位辅助函数：无需进位部分。
+  static long long upper_half(long long);  // 进位辅助函数：待进位部分。
+  void trim();
+  // 返回abs(left) >= abs(right)的真假性
+  friend bool no_lesser_abs(const int2048 &, const int2048 &);
+  // 要求 abs(left) >= abs(right). 返回 abs(left) - abs(right)，符号继承left
+  void minus_abs(const int2048 &);
+  // FFT
+  void FFT(std::vector<Complex> &, int, int);
+  long long pow_mod(int, int, long long);
+  long long mult_mod(long long, long long, long long);
+  void NTT(std::vector<long long> &, int, int, long long);
+
+  int2048 div_abs_brute(const int2048 &, const int2048 &) const;
+  // shift left/times BASE^shift. use negative param to shift right/divide by BASE^shift.
+  void shift(int shift);
+  // return BASE^size / this
+  int2048 inv();
+  // {quo, res}
+  std::pair<int2048, int2048> div_abs(const int2048 &) const;
+
   // 构造函数
+  // 无参数初始化为 0
   int2048();
   int2048(long long);
+  // 允许前导零
   int2048(const std::string &);
   int2048(const int2048 &);
+  ~int2048();
 
   // 以下给定函数的形式参数类型仅供参考，可自行选择使用常量引用或者不使用引用
   // 如果需要，可以自行增加其他所需的函数
@@ -51,6 +94,10 @@ public:
   // ===================================
   // Integer2
   // ===================================
+
+  bool operator==(const int2048 &);
+  bool operator<(const int2048 &);
+  bool operator>(const int2048 &);
 
   int2048 operator+() const;
   int2048 operator-() const;
@@ -81,6 +128,8 @@ public:
   friend bool operator>(const int2048 &, const int2048 &);
   friend bool operator<=(const int2048 &, const int2048 &);
   friend bool operator>=(const int2048 &, const int2048 &);
+
+
 };
 } // namespace sjtu
 
